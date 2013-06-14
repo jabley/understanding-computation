d1ca6659871830e40c0cfb204e8c16f2c931ee92
James Abley <james.abley@gmail.com>
2013-06-14 08:06:29 +0100

    Add Sequence
    This gives us the ability to compose Statements; eg
    
    >> Machine.new(
      Sequence.new(
        Assign.new(:x, Add.new(Number.new(1), Number.new(1))),
        Assign.new(:y, Add.new(Variable.new(:x), Number.new(3)))
      ),
      {}
    ).run

--- simple.rb
+++ simple.rb
@@ -183,6 +183,30 @@ class If < Struct.new(:condition, :consequence, :alternative)
   end
 end
 
+class Sequence < Struct.new(:first, :second)
+  def to_s
+    "#{first}; #{second}"
+  end
+
+  def inspect
+    "<#{self}>"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce(environment)
+    case first
+    when DoNothing.new
+      [second, environment]
+    else
+      reduced_first, reduced_enviroment = first.reduce(environment)
+      [Sequence.new(reduced_first, second), reduced_enviroment]
+    end
+  end
+end
+
 class Machine < Struct.new(:statement, :environment)
   def step
     self.statement, self.environment = statement.reduce(environment)
