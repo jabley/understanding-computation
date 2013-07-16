177b51837f4037d62952b59f43283fa2bbb44bc1
James Abley <james.abley@gmail.com>
2013-07-16 07:57:04 +0100

    Add DFADesign
    > dfa_design = DFADesign.new(1, [3], rulebook)
    > dfa_design.accepts?('a')
    > dfa_design.accepts?('baa')
    > dfa_design.accepts?('baba')

--- dfa.rb
+++ dfa.rb
@@ -35,3 +35,12 @@ class DFA < Struct.new(:current_state, :accept_states, :rulebook)
     end
   end
 end
+
+class DFADesign < Struct.new(:start_state, :accept_states, :rulebook)
+  def to_dfa
+    DFA.new(start_state, accept_states, rulebook)
+  end
+  def accepts?(string)
+    to_dfa.tap { |dfa| dfa.read_string(string) }.accepting?
+  end
+end
