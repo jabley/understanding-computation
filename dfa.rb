c725f216724303466c38de2085c52550dcbb1da0
James Abley <james.abley@gmail.com>
2013-07-16 07:53:23 +0100

    Add DFA class
    > dfa = DFA.new(1, [3], rulebook)
    > dfa.read_string('baaab')
    > dfa.accepting?

--- dfa.rb
+++ dfa.rb
@@ -21,3 +21,17 @@ class DFARulebook < Struct.new(:rules)
     rules.detect { |rule| rule.applies_to?(state, character) }
   end
 end
+
+class DFA < Struct.new(:current_state, :accept_states, :rulebook)
+  def accepting?
+    accept_states.include?(current_state)
+  end
+  def read_character(character)
+    self.current_state = rulebook.next_state(current_state, character)
+  end
+  def read_string(string)
+    string.chars.each do |character|
+      read_character(character)
+    end
+  end
+end
