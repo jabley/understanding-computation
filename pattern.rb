4856aabe91505b92d46f796ff2d6e647870d2455
James Abley <james.abley@gmail.com>
2013-07-25 18:23:32 +0100

    Convert an NFA to a DFA
    > rulebook = NFARulebook.new([
      FARule.new(1, 'a', 1), FARule.new(1, 'a', 2), FARule.new(1, nil, 2),
      FARule.new(2, 'b', 3),
      FARule.new(3, 'b', 1), FARule.new(3, nil, 2)
    ])
    > nfa_design = NFADesign.new(1, [3], rulebook)
    > simulation = NFASimulation.new(nfa_design)
    > dfa_design = simulation.to_dfa_design
    > dfa_design.accepts?('aaa')
    > dfa_design.accepts?('aab')
    > dfa_design.accepts?('bbabb')

--- pattern.rb
+++ pattern.rb
@@ -281,4 +281,12 @@ class NFASimulation < Struct.new(:nfa_design)
       discover_states_and_rules(states + more_states)
     end
   end
+
+  def to_dfa_design
+    start_state = nfa_design.to_nfa.current_states
+    states, rules = discover_states_and_rules(Set[start_state])
+    accept_states = states.select { |state| nfa_design.to_nfa(state).accepting? }
+
+    DFADesign.new(start_state, accept_states, DFARulebook.new(rules))
+  end
 end
