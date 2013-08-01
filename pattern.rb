55e0d61b87c127565d03338b858c93fb2bfe25a6
James Abley <james.abley@gmail.com>
2013-08-01 19:41:02 +0100

    Add Stack class

--- pattern.rb
+++ pattern.rb
@@ -290,3 +290,21 @@ class NFASimulation < Struct.new(:nfa_design)
     DFADesign.new(start_state, accept_states, DFARulebook.new(rules))
   end
 end
+
+class Stack < Struct.new(:contents)
+  def push(character)
+    Stack.new([character] + contents)
+  end
+
+  def pop
+    Stack.new(contents.drop(1))
+  end
+
+  def top
+    contents.first
+  end
+
+  def inspect
+    "#<Stack (#{top})#{contents.drop(1).join}>"
+  end
+end
