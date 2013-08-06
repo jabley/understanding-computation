ea0d7dd6730e002d983da8e9bc84fc1825f6284e
James Abley <james.abley@gmail.com>
2013-08-06 08:40:32 +0100

    Add PDAConfiguration and PDARule
    > rule = PDARule.new(1, '(', 2, '$', ['b', '$'])
    > configuration = PDAConfiguration.new(1, Stack.new(['$']))
    > rule.applies_to?(configuration, '(')

--- pattern.rb
+++ pattern.rb
@@ -308,3 +308,15 @@ class Stack < Struct.new(:contents)
     "#<Stack (#{top})#{contents.drop(1).join}>"
   end
 end
+
+class PDAConfiguration < Struct.new(:state, :stack)
+end
+
+class PDARule < Struct.new(:state, :character, :next_state,
+                          :pop_character, :push_characters)
+  def applies_to?(configuration, character)
+    self.state == configuration.state &&
+      self.pop_character == configuration.stack.top &&
+      self.character == character
+  end
+end
